81151fa6674564e06081d79993aad593d562a7b2
carlkuesters <carl.kuesters@youengineering.com>
2021-07-27 23:21:28 +0200

    Fix deploy script and comment out non-existing favicon.

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ VERSION=$1
 TARGET=$2
 
 # Checkout
-git clone https://github.com/carlkuesters/anselm-kuesters-frontend.git
+git clone https://github.com/carlkuesters/fifa-champions-frontend.git
 if [ -n "$VERSION" ]; then
   git checkout "$VERSION"
 fi
