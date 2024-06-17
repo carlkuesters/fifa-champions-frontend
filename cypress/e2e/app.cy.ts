eec8241e53b6293cc1c6125a33cac6b326c10c82
Carl Küsters <carl.kuesters@gmail.com>
2024-06-17 18:43:45 +0200

    Container
    Container

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -1,6 +1,6 @@
 describe('FifaChampionsApp', () => {
   it('should display copyright', () => {
     cy.visit('/')
-    cy.get('.copyright').contains('Copyright © 2023 FIFA-Champions')
+    cy.get('.copyright').contains('Copyright © 2024 FIFA-Champions')
   })
 })
